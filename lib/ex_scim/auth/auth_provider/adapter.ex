17d931879da7c3e01211ed8f03e69f88700e9a6e
LarsO <lars.oehnenamen@gmail.com>
2026-01-30 17:51:49 +0200

    feat!: update SCIM principal struct + add helper
    Update the principle struct:
    - Remove type field
    - Include metadata
    
    Add helper to create principle struct and check the scopes.

--- lib/ex_scim/auth/auth_provider/adapter.ex
+++ lib/ex_scim/auth/auth_provider/adapter.ex
@@ -3,9 +3,17 @@ defmodule ExScim.Auth.AuthProvider.Adapter do
 
   alias ExScim.Auth.Principal
 
+  @type auth_error ::
+          :invalid_credentials
+          | :token_not_found
+          | :expired_token
+          | :inactive_token
+          | :invalid_basic_format
+          | atom()
+
   @callback validate_bearer(token :: String.t()) ::
-              {:ok, Principal.t()} | {:error, term()}
+              {:ok, Principal.t()} | {:error, auth_error()}
 
   @callback validate_basic(username :: String.t(), password :: String.t()) ::
-              {:ok, Principal.t()} | {:error, term()}
+              {:ok, Principal.t()} | {:error, auth_error()}
 end
