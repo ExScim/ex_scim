1f6830a6905cf36097f9f365faf85b3b92abad10
Lars Oehnenamen <wheredoipressnow@users.noreply.github.com>
2026-02-06 10:52:21 +0200

    Add full namespace to resource protocol implementations

--- apps/ex_scim/lib/ex_scim/groups/resource_impl.ex
+++ apps/ex_scim/lib/ex_scim/groups/resource_impl.ex
@@ -1,9 +1,7 @@
 defmodule ExScim.Groups.ResourceImpl do
-  alias ExScim.Groups.Group
-
-  defimpl ExScim.Resources.Resource, for: Group do
-    def get_id(%Group{id: id}), do: id
-    def get_external_id(%Group{external_id: external_id}), do: external_id
-    def set_id(%Group{} = group, id), do: %{group | id: id}
+  defimpl ExScim.Resources.Resource, for: ExScim.Groups.Group do
+    def get_id(%ExScim.Groups.Group{id: id}), do: id
+    def get_external_id(%ExScim.Groups.Group{external_id: external_id}), do: external_id
+    def set_id(%ExScim.Groups.Group{} = group, id), do: %{group | id: id}
   end
 end
--- apps/ex_scim/lib/ex_scim/users/resource_impl.ex
+++ apps/ex_scim/lib/ex_scim/users/resource_impl.ex
@@ -1,9 +1,7 @@
 defmodule ExScim.Users.ResourceImpl do
-  alias ExScim.Users.User
-
-  defimpl ExScim.Resources.Resource, for: User do
-    def get_id(%User{id: id}), do: id
-    def get_external_id(%User{external_id: external_id}), do: external_id
-    def set_id(%User{} = user, id), do: %{user | id: id}
+  defimpl ExScim.Resources.Resource, for: ExScim.Users.User do
+    def get_id(%ExScim.Users.User{id: id}), do: id
+    def get_external_id(%ExScim.Users.User{external_id: external_id}), do: external_id
+    def set_id(%ExScim.Users.User{} = user, id), do: %{user | id: id}
   end
 end
