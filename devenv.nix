35bc77b0001a754ec8112b28b98af527300c0f54
Lars Oehnenamen <wheredoipressnow@users.noreply.github.com>
2026-06-09 20:27:45 +0300

    Upgrade Elixir to 1.20.1 and pin Expert-ls to 0.1.4

--- devenv.nix
+++ devenv.nix
@@ -13,12 +13,21 @@
     pkgs.tailwindcss-language-server
     pkgs.tailwindcss_4
     pkgs.nodejs
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.prettier
+    pkgs.vscode-langservers-extracted
+    pkgs.prettier
+    inputs.expert.packages.${pkgs.system}.default
   ];
 
   languages.elixir.enable = true;
-  languages.elixir.package = pkgs.beam27Packages.elixir_1_19;
+  languages.elixir.package = pkgs.beam28Packages.elixir_1_20.overrideAttrs {
+    version = "1.20.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "elixir-lang";
+      repo = "elixir";
+      rev = "v1.20.1";
+      hash = "sha256-eOYqYcZpHJqgbut0iOrey6CMD3LIvpqc3AU9L/g7a+Y=";
+    };
+  };
   languages.javascript.enable = true;
 
   env.TAILWINDCSS_PATH = "${pkgs.lib.getExe pkgs.tailwindcss_4}";
